594307241958962de25a104e7d8827d6b3d464dd
VasBel <vasilii.belyachkov@gmail.com>
2023-02-06 13:56:40 +0300

    feat:towel sort v2.0

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@
 
 module.exports = function towelSort (matrix) {
   let result = [];
-  if(matrix == undefined){
-    return [];
-  } else{
-    for(let i = 0; i < matrix.length; i++){
-      if(i%2 == 0){
-        result = [...result, ...matrix[i]];
-      } else{
-        result = [...result, ...matrix[i].reverse()];
-      }
-    }
-    return result;
+  if(matrix == undefined) return [];
+  for(i = 0; i < matrix.length; i++){
+    if(i%2 == 0) result = [...result,...matrix[i]];
+    else result = [...result,...matrix[i].reverse()];
   }
   
+  return result;
 }
 
 /* console.log(module.exports([
